62260f062ea3db2b744fb12fa9a300a2206825b4
jpfuentesv89 <jua.fuentes@duocuc.cl>
2023-03-12 20:44:03 -0300

    G1-12-03-2023

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Geolocation } from '@capacitor/geolocation';
-import { LoadingController } from '@ionic/angular';  
-import { AlertController } from '@ionic/angular';  
+import { LoadingController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -9,28 +9,43 @@ import { AlertController } from '@ionic/angular';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  latitude: any;
-  longitude: any;
-
-
-  constructor(public loadingController: LoadingController, public alertController: AlertController) {
-    this.getCurrentLocation();
+  ticket = {
+    cliente: {
+      rut: '',
+      nombre: '',
+      direccion: '',
+      comuna: '',
+      telefono: ''
+    },
+    fecha: '',
+    hora: '',
+    kilometraje: '',
+    atencion: {
+      pos: '',
+      tipo: '',
+      serieAtendida: '',
+      serieSaliente: '',
+      serieEntrante: '',
+      descripcion: '',
+      observaciones: ''
+    },
+    gps: {
+      longitud: 0,
+      latitud: 0
+    },
+    foto: ''
   }
 
-  async getCurrentLocation() {
-    const loading = await this.loadingController.create({
-      message: 'Porfavor espera...'
-    });
-    await loading.present();
-    Geolocation.getCurrentPosition( { maximumAge: 1000, timeout: 10000, enableHighAccuracy: true } ).then((resp) => {
-      this.latitude = resp.coords.latitude;
-      this.longitude = resp.coords.longitude;
-      loading.dismiss();
-    }).catch((error) => {
-      console.log('Error al obtener la ubicación', error);
-      loading.dismiss();
-      this.presentAlert('Sin cobertura GPS o GPS desactivado, favor revisar')
-    });
+  data: any;
+
+  constructor(public loadingController: LoadingController, public alertController: AlertController) {
+
+    this.guardarUbicacion('');
+
+  }
+
+  combustibleParada() {
+    this.guardarUbicacion('como parada de combustible');
   }
 
   async presentAlert(msg: string) {
@@ -41,6 +56,58 @@ export class HomePage {
     });
 
     await alert.present();
+  }
+
+  async guardarUbicacion(msg: string) {
+    const loading = await this.loadingController.create({
+      message: 'Porfavor espera...'
+    });
+    await loading.present();
+    Geolocation.getCurrentPosition({ maximumAge: 1000, timeout: 10000, enableHighAccuracy: true }).then((resp) => {
+      this.ticket.gps.latitud = resp.coords.latitude;
+      this.ticket.gps.longitud = resp.coords.longitude;
+      console.log(this.ticket.gps.latitud + ' ---> ' + this.ticket.gps.longitud)
+      console.log('Ubicación encontrada y guardada ');
+      loading.dismiss();
+      if (msg != '') {
+        this.presentAlert('Ubicación guardada ' + msg + ' con éxito');
+      }
+    }).catch((error) => {
+      console.log('Error al obtener la ubicación', error);
+      loading.dismiss();
+      this.presentAlert('Sin cobertura GPS o GPS desactivado, favor revisar');
+    });
+  }
+
+  async selectFile(event: any) {
+    const loading = await this.loadingController.create({
+      message: 'Porfavor espera...'
+    });
+    await loading.present();
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.readAsText(file);
+    reader.onload = () => {
+      console.log(reader.result);
+      this.data = reader.result?.toString().replace(/(\r\n|\n|\r)/gm, ';').split(';');
+      for (let index = 0; index < this.data.length;) {
+        this.ticket.atencion.pos = this.data[index + 6];
+        this.ticket.cliente.rut = this.data[index + 3];
+        this.ticket.cliente.nombre = this.data[index];
+        this.ticket.cliente.direccion = this.data[index + 1];
+        this.ticket.cliente.comuna = this.data[index + 2];
+        this.ticket.cliente.telefono = 'Sin telefono';
+        this.ticket.atencion.serieEntrante = this.data[index + 5];
+        index += 7;
+        console.log(this.ticket);
+      }
+
+      loading.dismiss();
+    };
+    reader.onerror = () => {
+      console.log('ocurrio un error al leer el archivo');
+      loading.dismiss();
+    };
   }
 
 }
